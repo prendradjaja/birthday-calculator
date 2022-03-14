86d4b9b92c5527dca05e94d55377a5db4fdb3d03
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-03-14 12:26:48 -0500

    MVP

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,61 @@
+import { readFileSync } from "fs";
+
+import { DateTime } from "luxon";
+
 import { hello } from "./hello";
 
-console.log(hello());
+function main() {
+  const examples = parseTsv(readFileSync("./examples.txt", "utf-8"), [
+    "name",
+    "bdayString",
+  ]);
+
+  const milestones = parseTsv(
+    `
+
+{"days": 10000}	10K days (~27 years)
+{"days": 16384}	2^14 days (~45 years)
+
+{"seconds": 1e9}	1B seconds (~32 years)
+{"seconds": 1073741824}	2^30 seconds (~34 years)
+
+`,
+    ["plusObjString", "description"]
+  );
+
+  for (let example of examples) {
+    console.log("\n\n" + example.name);
+    const bday = DateTime.fromISO(example.bdayString);
+
+    for (let milestone of milestones) {
+      if (milestone.plusObjString) {
+        const plusObj = JSON.parse(milestone.plusObjString);
+        const description = milestone.description ?? milestone.plusObjString;
+        const formattedDate = bday.plus(plusObj).toISODate();
+        console.log(`- ${formattedDate} ${description}`);
+      } else {
+        console.log();
+      }
+    }
+  }
+}
+
+function parseTsv(text: string, fieldNames: string[]) {
+  const result: any = [];
+  text
+    .trim()
+    .split("\n")
+    .map((line) => line.split("\t"))
+    .forEach((line) => {
+      const fields = line;
+      const record: any = {};
+      fields.forEach((value, i) => {
+        const fieldName = fieldNames[i];
+        record[fieldName] = value;
+      });
+      result.push(record);
+    });
+  return result;
+}
+
+main();
