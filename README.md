3c6a56af80c783bb25e0635923d4ed288920a62e
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-03-14 13:12:08 -0500

    Add more milestones; Add instructions

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-# Install
+# Install (as an end user)
 
 ```
 npm install
+```
+
+# Install (as a developer)
+
+Do the above, but also:
+
+```
 npm run prepare-husky
+```
+
+# Run
+
+```
+npm run build
+node dist/index.js examples.txt
 ```
--- src/index.ts
+++ src/index.ts
@@ -2,45 +2,64 @@ import { readFileSync } from "fs";
 
 import { DateTime } from "luxon";
 
-import { hello } from "./hello";
-
 function main() {
-  const examples = parseTsv(readFileSync("./examples.txt", "utf-8"), [
-    "name",
-    "bdayString",
+  if (process.argv.length < 3) {
+    console.log("Path to birthdays file required. Try using examples.txt");
+    return;
+  }
+  const birthdays = parseTsv(readFileSync(process.argv[2], "utf-8"), [
+    "person",
+    "dateString",
   ]);
+
+  // Sidereal year in Earth days according to Wikipedia (what's a sidereal year?).
+  // Three sigfigs is enough to calculate precisely (to the day) within a human lifetime.
+  const MARS_YEAR = 686.98;
 
   const milestones = parseTsv(
     `
-
-{"days": 10000}	10K days (~27 years)
-{"days": 16384}	2^14 days (~45 years)
-
-{"seconds": 1e9}	1B seconds (~32 years)
-{"seconds": 1073741824}	2^30 seconds (~34 years)
-
-`,
-    ["plusObjString", "description"]
+      {}	Born
+      {"months": 100}	100  months (~8 years)
+      {"days": ${MARS_YEAR * 10}}	10   Martian years (~19 years)
+      {"weeks": 1000}	1K   weeks (~19 years)
+      {"weeks": 1024}	2^10 weeks (~20 years)
+      {"days": 10000}	10K  days (1K tendays) (~27 years)
+      {"days": 11111}	11111 days (~30 years)
+      {"days": 11235}	11235 days (~31 years)
+      {"seconds": 1e9}	1B   seconds (~32 years)
+      {"days": 12345}	12345 days (~34 years)
+      {"days": ${MARS_YEAR * 18}}	18   Martian years (~34 years)
+      {"seconds": 1073741824}	2^30 seconds (~34 years)
+      {"weeks": 2000}	1K   fortnights (~38 years)
+      {"weeks": 2048}	2^10 fortnights (~39 years)
+      {"days": ${MARS_YEAR * 21}}	21   Martian years (~40 years)
+      {"days": 16384}	2^14 days (~45 years)
+    `,
+    ["durationString", "description"]
   );
 
-  for (let example of examples) {
-    console.log("\n\n" + example.name);
-    const bday = DateTime.fromISO(example.bdayString);
+  for (let [i, birthday] of birthdays.entries()) {
+    console.log(birthday.person);
+    const bday = DateTime.fromISO(birthday.dateString);
 
     for (let milestone of milestones) {
-      if (milestone.plusObjString) {
-        const plusObj = JSON.parse(milestone.plusObjString);
-        const description = milestone.description ?? milestone.plusObjString;
+      if (milestone.durationString) {
+        const plusObj = JSON.parse(milestone.durationString);
+        const description = milestone.description ?? milestone.durationString;
         const formattedDate = bday.plus(plusObj).toISODate();
-        console.log(`- ${formattedDate} ${description}`);
+        console.log(`* ${formattedDate}   ${description}`);
       } else {
         console.log();
       }
+    }
+    if (i !== birthdays.length - 1) {
+      console.log("\n");
     }
   }
 }
 
 function parseTsv(text: string, fieldNames: string[]) {
+  // TODO Do this FP style
   const result: any = [];
   text
     .trim()
@@ -51,7 +70,7 @@ function parseTsv(text: string, fieldNames: string[]) {
       const record: any = {};
       fields.forEach((value, i) => {
         const fieldName = fieldNames[i];
-        record[fieldName] = value;
+        record[fieldName] = value.trim();
       });
       result.push(record);
     });
